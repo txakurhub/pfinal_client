66459480afc1518dc61c72e5c52bd09f56f47d0e
YoelFerreyra <89047058+YoelFerreyra@users.noreply.github.com>
2022-06-20 18:26:07 -0300

    cuarto comentario

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
+import { Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home'
+import Landing from './components/LandingPage/Landing'
 
 function App() {
   return (
     <div className="App">
-      <Route exact path='/' component={LandingPage}></Route>
+      <Route exact path='/' component={Landing}></Route>
       <Route exact path='/home' component={Home}></Route>
     </div>
   );
