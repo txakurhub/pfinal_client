39be433693e41bcd0dd0687a0b5e889c8e1111c1
YoelFerreyra <89047058+YoelFerreyra@users.noreply.github.com>
2022-06-20 17:34:26 -0300

    comentario

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path='/' component={Landing}></Route>
+      <Route exact path='/home' component={Home}></Route>
     </div>
   );
 }
