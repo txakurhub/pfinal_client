a8cc286667b9e274c28e8fa98005c7016b828ed4
Leandro Pereyra <me.leandrop@gmail.com>
2022-06-21 13:03:49 -0300

    brand filter 1 - sin agregar a ninguna page o component

---
+++ src/components/BrandFilter.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { filterByBrand } from "../redux/actions";
+
+export default function BrandFilter({ setCurrentPage }) {
+  const dispatch = useDispatch();
+  const allProducts = useSelector((state) => state.allProducts);
+
+  let brandsName = allProducts.map((t) => t.brand).sort();
+
+  function handleFilterTemp(e) {
+    console.log(e.target.value);
+    dispatch(filterByBrand(e.target.value));
+    setCurrentPage(1);
+  }
+  return (
+    <select onChange={(e) => handleFilterTemp(e)}>
+      <option value="All">All Brands</option>
+      {brandsName.map((b) => {
+        return (
+          <option value={b} key={brandsName.indexOf(b)}>
+            {b}
+          </option>
+        );
+      })}
+    </select>
+  );
+}
