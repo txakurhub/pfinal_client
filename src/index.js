1b0cd028275e6008099f99932065e0f3b9f72f4f
LucianoScaglione <lucianoscaglione21@gmail.com>
2022-06-21 01:39:28 -0300

    NavBar terminado

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
@@ -17,7 +16,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Card from "../components/Card";
 import { getProducts } from "../redux/actions";
+import NavBar from '../components/NavBar'
 
 export default function Home() {
   const dispatch = useDispatch();
@@ -14,18 +15,19 @@ export default function Home() {
   return (
     <main>
       <section>
+        <NavBar />
         <div>
           {products
             ? products.map((p) => (
-                <Card
-                  key={p.id}
-                  title={p.title}
-                  image={p.image}
-                  brand={p.brand}
-                  model={p.model}
-                  price={p.price}
-                />
-              ))
+              <Card
+                key={p.id}
+                title={p.title}
+                image={p.image}
+                brand={p.brand}
+                model={p.model}
+                price={p.price}
+              />
+            ))
             : "No hay nada"}
         </div>
       </section>
