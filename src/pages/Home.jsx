72c484ee2b251ee012626f84ed59bd9baded6838
Leandro Pereyra <me.leandrop@gmail.com>
2022-06-21 10:53:47 -0300

    tailwind en dependencies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router";
 import Card from "../components/Card";
 import { getProducts } from "../redux/actions";
 
 export default function Home() {
+const history = useHistory()
   const dispatch = useDispatch();
   const products = useSelector((state) => state.allProducts);
 
@@ -11,6 +13,11 @@ export default function Home() {
     dispatch(getProducts());
   }, [dispatch]);
   console.log(products);
+  function handleClick(e){
+    e.preventDefault()
+    history()
+  }
+
   return (
     <main>
       <section>
@@ -18,7 +25,8 @@ export default function Home() {
           {products
             ? products.map((p) => (
                 <Card
-                  key={p.id}
+                onClick={handleClick}
+                key={p.id}
                   title={p.title}
                   image={p.image}
                   brand={p.brand}
