8d43907bdcf8167d0ae3296722b66b5c6eeef49f
Leandro Pereyra <me.leandrop@gmail.com>
2022-06-20 21:09:06 -0300

    page detail + actions & reducer

---
+++ src/pages/Detail.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router";
+import { getShoeDetail } from "../redux/actions";
+
+export default function Detail({ title, image, brand, model, price }) {
+  const dispatch = useDispatch();
+  const params = useParams();
+  const [loader, setLoader] = useState(true);
+  let selected = useSelector((state) => state.product_detail);
+
+  useEffect(() => {
+    dispatch(getShoeDetail(params.id)).then(() => setLoader(false));
+  }, [dispatch, params.id]);
+
+  if (loader === true) {
+    return <div>Acá va un loader...</div>;
+  }
+  return (
+    <main>
+      <div>
+        <img src={selected.image} alt={title} />
+      </div>
+      <div>
+        <h2>{selected.title}</h2>
+        <h4>{selected.brand}</h4>
+        <h4>{selected.model}</h4>
+        <h5>{selected.price}</h5>
+      </div>
+    </main>
+  );
+}
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,19 +1,27 @@
-import {
-    GET_PRODUCTS
-} from "./actions";
+import { GET_PRODUCTS, GET_SHOE_DETAIL } from "./actions";
 
-const initialState ={
-    products:[],
-    allProducts:[]
+const initialState = {
+  product_detail: {},
+  products: [],
+  allProducts: [],
+};
+
+function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_PRODUCTS:
+      return {
+        ...state,
+        products: action.payload,
+        allProducts: action.payload,
+      };
+    case GET_SHOE_DETAIL:
+      return {
+        ...state,
+        product_detail: action.payload,
+      };
+    default:
+      return { ...state };
+  }
 }
 
-function rootReducer(state=initialState,action){
-    switch(action.type){
-        case GET_PRODUCTS:
-            return{
-                ...state,
-                products:action.payload,
-                allProducts:action.payload
-            }
-    }
-}
+export default rootReducer;
