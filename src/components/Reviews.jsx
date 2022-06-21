8a2d6e0483020c49a3dd7df27be573abb592a74d
fvaron69 <fredyalberbaron@hotmail.com>
2022-06-21 14:21:42 -0500

    Reviews and Wishlist

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,9 +1,15 @@
 function Reviews() {
     return (
         <div>
-            
+            <form>
+                <label htmlFor="comentario"></label>
+                <input type="text" placeholder="Comentario" id="comentario"/>
+                <br />
+                <button type="submit">Enviar</button>
+            </form>
         </div>
     )
 }
 
 export default Reviews;
+
