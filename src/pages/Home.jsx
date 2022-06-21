a42450cc1b086a29ead9fa5439dbad7c42639445
LucianoScaglione <lucianoscaglione21@gmail.com>
2022-06-21 14:01:03 -0300

    Filter by price

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,26 +2,28 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Card from "../components/Card";
 import { getProducts } from "../redux/actions";
-import NavBar from '../components/Navbar'
+import NavBar from '../components/NavBar'
 import Paginado from "../components/Paginado";
-
+import FilterPrice from '../components/FilterPrice'
 
 export default function Home() {
+
   const dispatch = useDispatch();
+  const [order, setOrder] = useState('')
   const products = useSelector((state) => state.allProducts);
   const [currentPage, setCurrentPage] = useState(1);
- const productPage = 20;
+  const productPage = 20;
 
 
- const indexOfLastProduct = currentPage * productPage 
+  const indexOfLastProduct = currentPage * productPage
 
- const indexOfFirstProduct = indexOfLastProduct - productPage
+  const indexOfFirstProduct = indexOfLastProduct - productPage
 
- const currentProduct = products.slice(indexOfFirstProduct, indexOfLastProduct)
+  const currentProduct = products.slice(indexOfFirstProduct, indexOfLastProduct)
 
- const paginate = pageNumber =>{
-   setCurrentPage(pageNumber)
- }
+  const paginate = pageNumber => {
+    setCurrentPage(pageNumber)
+  }
 
   useEffect(() => {
     dispatch(getProducts());
@@ -30,27 +32,32 @@ export default function Home() {
   return (
     <main>
       <section>
-        <NavBar />
+        <NavBar
+          setCurrentPage={setCurrentPage} />
+        <FilterPrice
+          setCurrentPage={setCurrentPage}
+          setOrder={setOrder}
+          order={order} />
         <div>
           {currentProduct
             ? currentProduct.map((p) => (
-                <Card
-                  id={p.id}
-                  key={p.id}
-                  title={p.title}
-                  image={p.image}
-                  brand={p.brand}
-                  model={p.model}
-                  price={p.price}
-                />
-              ))
+              <Card
+                id={p.id}
+                key={p.id}
+                title={p.title}
+                image={p.image}
+                brand={p.brand}
+                model={p.model}
+                price={p.price}
+              />
+            ))
             : "No hay nada"}
-            <Paginado
-                  productPorPage={productPage}
-                  product={products.length}
-                  paginado={paginate}
-                  pagina={currentPage}
-            />
+          <Paginado
+            productPorPage={productPage}
+            product={products.length}
+            paginado={paginate}
+            pagina={currentPage}
+          />
         </div>
       </section>
     </main>
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,9 +1,9 @@
-import { GET_PRODUCTS, GET_SHOE_DETAIL, SEARCH_SNEAKES } from "./actions";
+import { GET_PRODUCTS, GET_SHOE_DETAIL, SEARCH_SNEAKES, FILTER_PRICE } from "./actions";
 
 const initialState = {
-  product_detail: [],
-  products: [],
   allProducts: [],
+  product_detail: [],
+  products: []
 };
 
 function rootReducer(state = initialState, action) {
@@ -12,7 +12,7 @@ function rootReducer(state = initialState, action) {
       return {
         ...state,
         products: action.payload,
-        allProducts: action.payload,
+        allProducts: action.payload
       };
     case GET_SHOE_DETAIL:
       return {
@@ -23,6 +23,30 @@ function rootReducer(state = initialState, action) {
       return {
         ...state,
         allProducts: action.payload
+      }
+    }
+    case FILTER_PRICE: {
+      const container = action.payload === 'lowest' ? state.allProducts.sort((a, b) => {
+        if (a.price > b.price) {
+          return 1
+        }
+        if (a.price < b.price) {
+          return -1
+        }
+        return 0
+      }) : state.allProducts.sort((a, b) => {
+        if (a.price > b.price) {
+          return -1
+        }
+        if (a.price < b.price) {
+          return 1
+        }
+        return 0
+      }
+      )
+      return {
+        ...state,
+        allProducts: container
       }
     }
     default:
