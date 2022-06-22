5964039580f684b40bf8ff16ca2528a7656d1a17
Leandro Pereyra <me.leandrop@gmail.com>
2022-06-21 22:24:26 -0300

    auth context, firebase-config y primer register en login

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import Dashboard from "./pages/Dashboard";
 import Detail from "./pages/Detail";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import ShoppingCart from "./pages/ShoppingCart"
+import ShoppingCart from "./pages/ShoppingCart";
 import Wishlist from "./pages/Wishlist";
+import { AuthProvider } from "./context/authContext";
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" component={Home}></Route>
-      <Route exact path="/:id" component={Detail}></Route>
-      <Route exact path="/login" component={Login}></Route>
-      <Route exact path="/:user/:id" component={Dashboard}></Route>
-      <Route exact path='/user/:id/cart' component={ShoppingCart}></Route>
-      <Route exact path='/user/:id/wishlist' component={Wishlist}></Route>
+      <AuthProvider>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/:id" component={Detail}></Route>
+        <Route exact path="/login" component={Login}></Route>
+        <Route exact path="/:user/:id" component={Dashboard}></Route>
+        <Route exact path="/user/:id/cart" component={ShoppingCart}></Route>
+        <Route exact path="/user/:id/wishlist" component={Wishlist}></Route>
+      </AuthProvider>
     </div>
   );
 }
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,13 +1,6 @@
-//https://firebase.google.com/docs/auth/web/start?authuser=3
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// import firebase from 'firebase'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAauuDZYUBgVdRjJYAQeKo47RzgmMe38M0",
   authDomain: "proyecto-zapatillas-6e2ce.firebaseapp.com",
@@ -18,8 +11,7 @@ const firebaseConfig = {
   measurementId: "G-62NF4KXSGW",
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
-// const analytics = getAnalytics(app);
+
 
