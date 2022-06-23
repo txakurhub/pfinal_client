7a63d0fa478d4ef6bf67c1f88b48cec67947f28a
Maxi <maximartins45@gmail.com>
2022-06-23 14:58:18 -0300

    Implemente el autocomplete en navbar
    cree un estado en el reducer para poder utilizarlo en el navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,33 @@ import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { searchSneakes } from '../redux/actions'
 
-const NavBar = ({ setCurrentPage }) => {
-
+const NavBar = ({ setCurrentPage , nombreProductos }) => {
+  // console.log(nombreProductos)
   const [input, setInput] = useState('')
   const dispatch = useDispatch()
+  const [suggestionsState,setSuggestionsState] = useState({mostrar:false,sugerencia:[]})
 
-  const handleChange = (e) => {
-    setInput(e.target.value)
+  const suggestions = (texto)=>{
+    if(nombreProductos.length){
+      if(!texto) {
+        setSuggestionsState({sugerencia:[],mostrar:false})
+        return
+      }
+      if(texto){
+        const filtrado = nombreProductos.filter(e=>`${e.title} ${e.brand}`.toLowerCase().includes(texto.toLowerCase()))
+        setSuggestionsState({mostrar:true,sugerencia:filtrado})
+      }
+    }
+  }
+
+  const handleClick = (e)=>{
+    setInput(e.target.id)
+    setSuggestionsState({sugerencia:[],mostrar:false})
+  }
+
+  const handleChange = async (e) => {
+    await setInput(e.target.value)
+    suggestions(e.target.value)
   }
 
   const handleSubmit = (e) => {
@@ -17,13 +37,19 @@ const NavBar = ({ setCurrentPage }) => {
     setInput('')
     setCurrentPage(1)
   }
-
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <input type='search' value={input} placeholder="Search sneakes..." onChange={handleChange} />
         <input type='submit' />
       </form>
+      {suggestionsState.mostrar && (
+        <div>
+          {suggestionsState.sugerencia.map((el,index)=>(
+        <p key={index} onClick={(e)=>handleClick(e)} id={el.title}>{`${el.title}`}</p>
+      ))}
+        </div>
+      )}
     </div>
   )
 }
