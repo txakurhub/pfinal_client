e9a334d838a4b0dcb25e653e1d6d7f958dc6da04
LucianoScaglione <lucianoscaglione21@gmail.com>
2022-06-24 19:45:43 -0300

    Filtros mejorados y terminados

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -6,12 +6,14 @@ export const GET_SHOE_DETAIL = "GET_SHOE_DETAIL";
 export const SEARCH_SNEAKES = "SEARCH_SNEAKES";
 export const FILTER_PRICE = "FILTER_PRICE";
 export const GET_CATEGORIES = "GET_CATEGORIES";
-export const FILTER_CATEGORY = "FILTER_CATEGORY";
 export const CREATE_REVIEW = "CREATE_REVIEW";
 export const GET_REVIEWS_PRODUCT = "GET_REVIEWS_PRODUCT";
+export const FILTER_CATEGORY = "FILTER_CATEGORY"
 export const CREATE_WISHLIST_PRODUCT = "CREATE_WISHLIST_PRODUCT";
 export const GET_WISHLIST_PRODUCT = "GET_WISHLIST_PRODUCT";
 export const REMOVE_PRODUCT_WISHLIST = "REMOVE_PRODUCT_WISHLIST";
+export const ALL_FILTERS = "ALL_FILTERS"
+
 
 export function getProducts() {
   return function (dispatch) {
@@ -48,6 +50,7 @@ export const create_new_review = (payload) => {
     );
   };
 };
+
 export const get_reviews = (id) => {
   console.log(id);
   return (dispatch) => {
@@ -62,16 +65,6 @@ export const filterByPrice = (payload) => {
   return {
     type: FILTER_PRICE,
     payload,
-  };
-};
-
-export const filterByRangePrice = (priceMin, priceMax) => {
-  return {
-    type: "FILTER_BY_RANGE_PRICE",
-    payload: {
-      priceMin,
-      priceMax,
-    },
   };
 };
 
@@ -101,13 +94,15 @@ export const createProduct = (payload) => {
     return json;
   };
 };
-export const get_wishlist_product = (payload) =>{
-  return (dispatch)=>{
+
+export const get_wishlist_product = (payload) => {
+  return (dispatch) => {
     axios.get(`${local_url}/wishlist/${payload}`)
-    .then((res)=> dispatch({type: GET_WISHLIST_PRODUCT, payload: res.data}),
-    (error) => alert("Error"))
+      .then((res) => dispatch({ type: GET_WISHLIST_PRODUCT, payload: res.data }),
+        (error) => alert("Error"))
   }
 }
+
 export const create_new_wishlist = (payload) => {
   return (dispatch) => {
     axios.post(`${local_url}/wishlist`, payload).then(
@@ -116,16 +111,24 @@ export const create_new_wishlist = (payload) => {
     );
   };
 };
-export const remove_wishlist_product = (id, id_user)=>{
-  const wish=  {
-    id_user: id_user, 
+
+export const remove_wishlist_product = (id, id_user) => {
+  const wish = {
+    id_user: id_user,
     id: id
-}
-console.log(wish)
-  return (dispatch)=>{
+  }
+  console.log(wish)
+  return (dispatch) => {
     axios.delete(`${local_url}/wishlist`, { data: { id: id, id_user: id_user } })
-    .then((res)=> dispatch({type: REMOVE_PRODUCT_WISHLIST, payload: res.data}),
-    (error) => alert(error)) 
+      .then((res) => dispatch({ type: REMOVE_PRODUCT_WISHLIST, payload: res.data }),
+        (error) => alert(error))
+  }
+}
+
+export const allFilters = (payload) => {
+  return {
+    type: ALL_FILTERS,
+    payload
   }
 }
 
