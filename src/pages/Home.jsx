36d302f93d4c76c7f8c3062cdbb06c2c3bf9b2c9
thomasHeitFux <thomas41392@gmail.com>
2022-06-24 13:37:58 -0300

    paginado 2.0

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, {useContext, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Card from "../components/Card";
 import { getCategories, getProducts } from "../redux/actions";
+// import NavBar from "../components/Navbar";
 import Paginado from "../components/Paginado";
+// import FilterPrice from "../components/FilterPrice";
 import PageHeading from "../components/PageHeading";
+import FilterCategory from "../components/FilterCategory";
 import { useAuth } from "../context/authContext";
 import { useHistory } from "react-router-dom";
 import NavBar from "../components/Navbar";
@@ -14,55 +17,88 @@ import NavBar from "../components/Navbar";
 
 export default function Home() {
   const dispatch = useDispatch();
-  const history = useHistory();
   const [order, setOrder] = useState("");
-  const [currentPage, setCurrentPage] = useState(1);
-  const { user, logout, loading } = useAuth();
   const nombreProductos = useSelector(state => state.allProductsName);
   const products = useSelector((state) => state.allProducts);
+  const [currentPage, setCurrentPage] = useState(1);
   const productPage = 20;
-  // const {addToCart} = useContext(CartContext)
+
   const indexOfLastProduct = currentPage * productPage;
+
   const indexOfFirstProduct = indexOfLastProduct - productPage;
-  const currentProduct = products.slice(indexOfFirstProduct, indexOfLastProduct);
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
-  
-  const handleLogin = () => history.push("/login");
-  
-  const handleLogout = async () => {
-    try {
-      await logout();
-      history.push("/");
-    } catch (error) {
-      console.log(error);
-    };
+
+  const currentProduct = products.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
   };
-    
+
   useEffect(() => {
     dispatch(getProducts());
     dispatch(getCategories());
   }, [dispatch]);
-  
+  const history = useHistory();
+  const handleLogout = async () => {
+    await logout();
+    history.push("/");
+  };
+  const handleLogin = () => {
+    history.push("/login");
+  };
+  const { user, logout, loading } = useAuth();
+  // console.log(user);
   return (
     <>
-      <NavBar nombreProductos={nombreProductos} setCurrentPage={setCurrentPage} loading={loading} user={user} handleLogin={handleLogin} handleLogout={handleLogout} />
-      {
-        currentProduct ?
-        <PageHeading products={products} setCurrentPage={setCurrentPage} setOrder={setOrder} order={order} /> : 
-        null
-      }
-      {
-        currentProduct ?
+      <NavBar setCurrentPage={setCurrentPage} />
+      {loading ? (
+        <div>
+          <h4>Loading...</h4>
+        </div>
+      ) : user ? (
+        <div>
+          <h4>Welcome {user.email}</h4>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <button onClick={handleLogin}>Login</button>
+        </div>
+      )}
+      <FilterCategory />
+      {currentProduct ? (
+        <PageHeading
+          products={products}
+          setCurrentPage={setCurrentPage}
+          setOrder={setOrder}
+          order={order}
+        />
+      ) : null}
+      {currentProduct ? (
         <div className="mt-10 grid lg:grid-cols-2 gap-x-8 gap-y-8 items-center px-[10px]">
-          {
-            currentProduct.map((r) => 
-              <Card id={r.id} key={r.id} title={r.title} image={r.image} brand={r.brand} model={r.model} price={r.price} product={r} />
-            )
-          }
-        </div> : 
+          {currentProduct.map((r) => (
+            <Card
+              id={r.id}
+              key={r.id}
+              title={r.title}
+              image={r.image}
+              brand={r.brand}
+              model={r.model}
+              price={r.price}
+            />
+          ))}
+        </div>
+      ) : (
         "No hay nada"
-      }
-      <Paginado productPorPage={productPage} product={products.length} paginado={paginate} pagina={currentPage} />
+      )}
+      <Paginado
+        productPorPage={productPage}
+        product={products.length}
+        paginado={paginate}
+        pagina={currentPage}
+      />
     </>
   );
 }
