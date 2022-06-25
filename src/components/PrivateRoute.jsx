0d66764abfba0199809b3a6513372b2fb510d728
LucianoScaglione <lucianoscaglione21@gmail.com>
2022-06-25 18:19:54 -0300

    checkout carrito solo para usuarios

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,25 @@
+//NO TOCAR ESTE COMPONENTE
+
+// import React from 'react'
+// import { Redirect, Route } from 'react-router-dom'
+// import { useAuth } from "../context/authContext";
+
+// const PrivateRoute = ({ component: Component, ...rest }) => {
+//     console.log({...rest})
+
+//     const { user } = useAuth();
+
+//     return (
+//         <Route {...rest}>{user ? <Component /> : <Redirect to="/login" />}</Route>
+//     )
+// }
+
+// export default PrivateRoute
+
+
+{/* <PrivateRoute exact path='/cart' component={PageShopingCart} /> */}
+
+
+
+// import { useAuth } from "../context/authContext";
+// const { user } = useAuth();
