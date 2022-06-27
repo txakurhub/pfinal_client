8cfa0493cd95d55e51e46c571da9a7405786be23
Leandro Pereyra <me.leandrop@gmail.com>
2022-06-26 21:49:13 -0300

    lalalala

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import swal from 'sweetalert';
-const local_url = "http://localhost:3001";
+const local_url = "https://zapatillas-proyecto.herokuapp.com";
 
 export const GET_PRODUCTS = "GET_PRODUCTS";
 export const GET_SHOE_DETAIL = "GET_SHOE_DETAIL";
