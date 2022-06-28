eb2eddad65ceb5e067a399ea8d4efca0ba3a92a8
Maximiliano <maximartins45@gmail.com>
2022-06-27 23:37:04 -0300

    Update Categories.jsx

--- src/pages/Dashboard/Categories.jsx
+++ src/pages/Dashboard/Categories.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
-import FilterSort from './FilterSort';
 
 function Categories() {
   const allcategories = useSelector(state=>state.categories)
