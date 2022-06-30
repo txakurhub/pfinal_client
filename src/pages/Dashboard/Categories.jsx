7f1c8e1224d0f95bee8a528a20bfbe5e1ab8d19c
Maximiliano <maximartins45@gmail.com>
2022-06-30 12:44:12 -0300

    Update Categories.jsx

--- src/pages/Dashboard/Categories.jsx
+++ src/pages/Dashboard/Categories.jsx
@@ -1,11 +1,17 @@
-import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import swal from 'sweetalert'
+import { getAllCategoryAdmin } from '../../redux/actions'
+
 
 function Categories() {
-  const allcategories = useSelector(state=>state.categories)
+  const allcategories = useSelector(state=>state.allcategoriesAdmin)
   const [categories,setCategories] = useState(allcategories)
   const [buscar,setBuscar] = useState("")
-
+  const [actualizar,setActualizar]=useState(true)
+  const history = useHistory()
+  const dispatch = useDispatch()
   const handleChange = (e)=>{
     setBuscar(e.target.value)
   }
@@ -17,18 +23,48 @@ function Categories() {
     setCategories(resultado)
   }
 
-  const deleteCategories = ({id,name})=>{
+  const deleteCategories = async ({id,name})=>{
+    await swal({
+      title: "¿Estás seguro de realizar esta acción?",
+      text: "¡Una vez eliminado, no podrá recuperar este producto!",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    })
+    .then(async (willDelete) => {
+      if (willDelete) {
+        swal(`¡El producto ${name}, ha sido eliminado correctamente!`, {
+          icon: "success",
+        });
+        //await dispatch(adminDeleteCategories(id));
+       setTimeout(() => {
+        window.location.reload()
+       }, 2000);
+      } else {
+        swal("¡La acción ha sido cancelada con éxito!");
+      }
+    })
     alert(`ELIMINAR => ID: ${id} NAME: ${name}`)
     // esta funcion va a despachar una accion que elimina el producto
   }
   const editCategories = ({id,name})=>{
-    alert(`EDITAR => ID: ${id} NAME: ${name}`)
-    // esta funcion va a despachar una accion que elimina el producto
+    history.push("/edit/category/"+id)
+    // esta funcion va a despachar una accion que edita el producto
   }
 
   const allCategories = ()=>{
     setCategories(allcategories)
   }
+  useEffect(() => {
+    if(actualizar){
+      dispatch(getAllCategoryAdmin())
+    }
+    setCategories(allcategories)
+    setActualizar(false)
+    
+  }, [allcategories])
+  
+
   return (
     <div>
         <div >
@@ -42,15 +78,17 @@ function Categories() {
           <tr>
             <th scope="col">ID</th>
             <th scope="col">CATEGORIA</th>
+            <th scope="col">CANTIDAD PRODUCTOS</th>
             <th scope="col">ACTION</th>
           </tr>
         </thead>
         <tbody>
-          {categories.map((category, index) => {
+          {categories&&categories.map((category, index) => {
             return (
               <tr key={category.id}>
                 <th scope="row">{category.id}</th>
                 <td className="text-dark"> {category.name}</td>
+                <td className="text-dark"> {category.cantidad}</td>
                 <td className="text-dark">
                   <div>
                     <button id={category.id} onClick={()=>editCategories({id:category.id,name:category.name})}>Editar</button>
