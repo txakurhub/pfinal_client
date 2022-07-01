d2bd338edc7834474c167cbc80ea2a637f913d81
thomasHeitFux <thomas41392@gmail.com>
2022-07-01 16:44:42 -0300

    vdash

---
+++ src/components/CategoryAdd.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom';
+import { addCategory } from '../redux/actions';
+
+export default function CategoryAdd() {
+
+  const dispatch = useDispatch()
+  const [info, setInfo] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addCategory(info))
+  };
+
+  const handleChange = (e) => {
+    e.preventDefault()
+    setInfo(e.target.value)
+    console.log(info)
+  }
+
+  return (
+    <div>
+      <Link to="/">
+        <button>Volver</button>
+      </Link>
+      <form onClick={(e) => handleSubmit(e)}>
+        <input type="text" name='name' value={info} placeholder='Nombre' onChange={(e) => handleChange(e)} />
+        <input type="submit" />
+      </form>
+    </div>
+  )
+}
