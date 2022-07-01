bb763305f2e27caf13ff2aad01a6201a97768123
Leandro Pereyra <me.leandrop@gmail.com>
2022-07-01 11:46:18 -0300

    fix carrousel width 100vp

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -42,7 +42,7 @@ export const Carrousel = () => {
   return (
     <div ref={slideRef} className="w-full select-none relative flex justify-center">
       <div className="aspect-w-16 aspect-h-9">
-        <img src={imagesArr[index]} alt="img carrousel" />
+        <img src={imagesArr[index]} alt="img carrousel" className="w-screen" />
       </div>
       <div className="absolute w-full top-1/2 transform -translate-y-1/2 py-2 px-3 flex justify-between items-center">
         <button onClick={handlePrev}>
