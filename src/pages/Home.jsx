0cb86cbc355337a3583a286ead0fe90136e8048d
General <joseluiscrrof@gmail.com>
2022-07-01 19:20:32 -0500

    QuickView, arreglando carrousel, navbar, modal, card y home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,16 +11,23 @@ import { ProductosDestacados } from "../components/ProductosDestacados";
 import { Carrousel } from "../components/Carrousel";
 // import ShoppingCart from '../components/ShoppingCart';
 // import { CartContext } from "../context/CartItem";
+import QuickView from "../components/QuickView";
+import Modal from "../components/Modal";
 
 export default function Home() {
   const dispatch = useDispatch();
   const history = useHistory();
   const [order, setOrder] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
+  const [id, setId] = useState('');
+  const [active, setActive] = useState(false);
+  const [product, setProduct] = useState();
+  const toggle = () => setActive(!active);
+  const onClick = (r) => setId(r);
   const { user, logout, loading } = useAuth();
   const nombreProductos = useSelector(state => state.allProductsName);
   const products = useSelector((state) => state.allProducts);
-  const productDestacados = useSelector((state)=> state.productosDestacados)
+  const productDestacados = useSelector((state)=> state.productosDestacados);
   const productPage = 20;
   const indexOfLastProduct = currentPage * productPage;
   const indexOfFirstProduct = indexOfLastProduct - productPage;
@@ -50,6 +57,7 @@ export default function Home() {
       dispatch(getProductosDestacados())
     }
   },[dispatch, productDestacados])
+
   return (
     <>
       <NavBar nombreProductos={nombreProductos} setCurrentPage={setCurrentPage} loading={loading} user={user} handleLogin={handleLogin} handleLogout={handleLogout} />
@@ -64,21 +72,22 @@ export default function Home() {
           <div className="mt-10 grid md:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3  gap-x-8 gap-y-8 items-center px-[10px]">
             {
               currentProduct.map((r) =>
-
-                r.stock !== 0 && <Card id={r.id} key={r.id} title={r.title} image={r.image} brand={r.brand} model={r.model} price={r.price} product={r} stock={r.stock} sold={r.sold} wishlist={r.wishlist} />
-
+                r.stock !== 0 && <Card setProduct={setProduct} toggle={toggle} onClick={onClick} id={r.id} key={r.id} title={r.title} image={r.image} brand={r.brand} model={r.model} price={r.price} product={r} stock={r.stock} sold={r.sold} wishlist={r.wishlist} />
               )
             }
           </div> :
           "Nothing"
       }
-     <h2>Productos Destacados</h2>
-      {
-        productDestacados?.map((r)=>(
-          r.stock !== 0 &&<ProductosDestacados title={r.title} image={r.image} brand={r.brand} model={r.model} />
-        ))
-      }
-      <Paginado productPorPage={productPage} product={products.length} paginado={paginate} pagina={currentPage} setPagina={setCurrentPage} />
+      <h2 className="mt-10 ml-5 text-2xl font-semibold leading-normal text-gray-800 flex justify-start">Productos Destacados</h2>
+      <div className="mt-3 grid md:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3  gap-x-8 gap-y-8 items-center px-[10px]">
+        {
+          productDestacados?.map((r)=>(
+            r.stock !== 0 && <Card setProduct={setProduct} toggle={toggle} onClick={onClick} id={r.id} key={r.id} title={r.title} image={r.image} brand={r.brand} model={r.model} price={r.price} product={r} stock={r.stock} sold={r.sold} wishlist={r.wishlist} />
+          ))
+        }
+      </div>
+      {currentProduct ? <Paginado productPorPage={productPage} product={products.length} paginado={paginate} pagina={currentPage} setPagina={setCurrentPage} /> : null}
+      <Modal active={active} toggle={toggle} children={<QuickView id={id} product={product} />} />
     </>
   );
-}
+};
