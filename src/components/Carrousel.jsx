4de009680deb9cc3e3ea7eec95dee779769b2d1b
Beristu4 <agus.beri17@gmail.com>
2022-07-02 14:50:44 -0300

    Carrosel

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState, useRef } from "react";
-import img1 from "../assets/1.jpg";
-import img2 from "../assets/2.jpg";
-import img3 from "../assets/3.jpg";
-import img4 from "../assets/4.jpg";
-import img5 from "../assets/5.jpg";
+import img1 from "../assets/zap1.jpg";
+import img2 from "../assets/zap2.jpg";
+import img3 from "../assets/zap3.jpg";
+import img4 from "../assets/zap4.jpg";
+import img5 from "../assets/zap5.jpg";
 let count = 0
 
 export const Carrousel = () => {
