9116792f32fb40a4dcf37782613758a7efba978e
Maximiliano <maximartins45@gmail.com>
2022-07-02 11:27:04 -0300

    Arreglar estilos de esto

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -15,10 +15,16 @@ function Dashboard() {
   
   return(
     <div className="w-full">
+      <div className="flex bg-gray-100 shadow-sm p-6">
       <Nav/> {/* va ir arriba */}
+      </div>
       <div className="flex h-screen">
+      <div className="px-3 w-90 ">
       <Sidebar setView={setView} view={view}/> {/* barra lateral */}
+       </div>
+      <div className="p-2">
       <Container active={view}/> {/* ocupa el resto de la pantalla */}
+      </div>
       </div>
     </div>
   )
