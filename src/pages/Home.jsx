4287c811ff093e1a5475a301a3e90cd92b9d496b
Thomas Dominguez <90942448+thomasHeitFux@users.noreply.github.com>
2022-07-02 02:03:51 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,7 +33,6 @@ export default function Home() {
   const indexOfFirstProduct = indexOfLastProduct - productPage;
   const currentProduct = products.slice(indexOfFirstProduct, indexOfLastProduct);
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
-console.log(user);
   const handleLogin = () => history.push("/login");
 
   const handleLogout = async () => {
