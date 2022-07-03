b8b87a635f6ebf428587e8ed4439c9cb322c052b
LucianoScaglione <lucianoscaglione21@gmail.com>
2022-07-03 00:46:34 -0300

    esto no

--- src/components/PrivateRouteAdmin.jsx
+++ src/components/PrivateRouteAdmin.jsx
@@ -1,23 +1,21 @@
 // import { useEffect } from 'react'
 // import { useDispatch, useSelector } from 'react-redux';
-// import { Redirect, Route } from 'react-router-dom'
-// import { getUsers } from '../redux/actions';
+// import { Redirect, Route, useParams } from 'react-router-dom'
+// import { getUser } from '../redux/actions';
 
 // const PrivateRouteAdmin = ({ component: Component, ...rest }) => {
+
+//     let { userId } = useParams()
+//     console.log(userId)
 //     const dispatch = useDispatch()
-//     const users = useSelector(state => state.users)
-//     console.log("usuarios:", users)
-//     const prueba = users.find(e => e.admin === true)
-//     console.log(prueba)
-//     console.log(users)
+//     const user = useSelector(state => state.user)
 
 //     useEffect(() => {
-//         dispatch(getUsers())
-//     }, [dispatch, users])
-
+//         dispatch(getUser({ id: userId.id }))
+//     }, [dispatch, user])
 
 //     return (
-//         <Route {...rest}>{prueba && <Component />}</Route>
+//         <Route {...rest}>{user.admin === true ? <Component /> : <Redirect to="/" />}</Route>
 //     )
 
 // }
