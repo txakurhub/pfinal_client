e640a23b40cd60e791cefc9fd27b0ca3a9fb42ca
Maximiliano <maximartins45@gmail.com>
2022-07-03 16:00:13 -0300

    pa que funque el panel de admin

--- src/pages/Dashboard/Container.jsx
+++ src/pages/Dashboard/Container.jsx
@@ -1,15 +1,32 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import OrderStatus from "../../components/OrderStatus";
 import Users from "../../components/Users";
 import Categories from "./Categories";
+import PanelAdmin from "./PanelAdmin";
 import Shoes from "./Shoes";
 
 const Container = ({ active }) => {
-    if(active === "customers") return <Users />;
-    if(active === "shoes") return <Shoes />;
-    if(active === "categories") return <Categories />;
-    if(active === "order") return <OrderStatus />;
-    if(active === "dashboard") return <div>Aca van datos del admin y datos informativo sobre la pagina</div>;
+  const { allProducts, users, orderstatus, allcategoriesAdmin } = useSelector(
+    (state) => state
+  );
+
+  if (active === "customers") return <Users />;
+  if (active === "shoes") return <Shoes />;
+  if (active === "categories") return <Categories />;
+  if (active === "order") return <OrderStatus />;
+  if (active === "dashboard") {
+    if (allProducts.length && users.length && orderstatus)
+      return (
+        <PanelAdmin
+          productData={allProducts}
+          userData={users}
+          categoryData={allcategoriesAdmin}
+          orderData={orderstatus}
+        />
+      );
+    else return <div>Espera que esta cargando capo</div>;
+  }
 };
 
 export default Container;
