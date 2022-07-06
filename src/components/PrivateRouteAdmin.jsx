a7599196f39e60995d153ba9faade1f779a4ec6b
LucianoScaglione <lucianoscaglione21@gmail.com>
2022-07-06 15:30:23 -0300

    no hay cambios por acá

--- src/components/PrivateRouteAdmin.jsx
+++ src/components/PrivateRouteAdmin.jsx
@@ -1,5 +1,6 @@
 import { Redirect, Route } from 'react-router-dom'
 import { useAuth } from '../context/authContext';
+import Error404 from './Error404'
 
 const PrivateRouteAdmin = ({ component: Component, ...rest }) => {
 
@@ -11,7 +12,7 @@ const PrivateRouteAdmin = ({ component: Component, ...rest }) => {
 
   return (
     // cambiar la varia de false a true
-    <Route {...rest}>{userInf && userInf.admin === "true" || userInf && userInf.admin === true ? <Component /> : <Redirect to="/" />}</Route>
+    <Route {...rest}>{userInf && userInf.admin === "true" || userInf && userInf.admin === true ? <Component /> : <Error404 />}</Route>
   )
 }
 
