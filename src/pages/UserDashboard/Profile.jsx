c854660ee4f197e109399e26fb928207b11d7deb
Leandro Pereyra <me.leandrop@gmail.com>
2022-07-08 21:17:18 -0300

    faltaban unos detalles

--- src/pages/UserDashboard/Profile.jsx
+++ src/pages/UserDashboard/Profile.jsx
@@ -6,19 +6,14 @@ export const Profile = ({ user }) => {
     <div className="flex flex-col justify-center w-100">
       <div className="rounded-full h-[450px] w-[450px] overflow-hidden m-auto my-2.5">
         <img src={user.image !== ""? user.image : user.photoURL ? user.photoURL : userimg}
-          className="self-center rounded-full " width={120} height={120}
-        />
+          className="h-full" />
       </div>
       <h1 className="text-4xl">
-        Hola{" "}
-        {user.firstname !== ""
-          ? user.firstname + " " + user.lastname
-          : user.displayName !== ""
-          ? user.displayName
-          : "usuario"}
+        Hola {user.firstname !== "" ? user.firstname + " " + user.lastname
+          : user.displayName !== "" ? user.displayName : "Usuario"}
       </h1>
-      <h2>telefono: {user.phone}</h2>
-      <h3>email:{user.email}</h3>
+      <h2>Tel: {user.phone}</h2>
+      <h3>Email:{user.email}</h3>
     </div>
   );
 };
