782134439bc250870979688da2973bf4a0c3b0fd
Maximiliano <maximartins45@gmail.com>
2022-07-12 12:38:05 -0300

    Arregle bucle en useEffecty arregle rutas protegid

--- src/components/PrivateRouteAdmin.jsx
+++ src/components/PrivateRouteAdmin.jsx
@@ -1,19 +1,19 @@
 import { Route } from 'react-router-dom'
 import { useAuth } from '../context/authContext';
 import Error404 from './Error404'
-import Loading from './Loading'
+// import Loading from './Loading'
 
 const PrivateRouteAdmin = ({ component: Component, ...rest }) => {
 
-  const { userInf } = useAuth();
+  const { userStorage } = useAuth(); 
 
-  if (!userInf) {
-    return <Loading />
-  }
+  // if (!userStorage) {
+  //   return <Loading />
+  // }
 
   return (
     // cambiar la varia de false a true
-    <Route {...rest}>{userInf && userInf.admin === "true" || userInf && userInf.admin === true ? <Component /> : <Error404 />}</Route>
+    <Route {...rest}>{userStorage.admin ? <Component /> : <Error404 />}</Route>
   )
 }
 
