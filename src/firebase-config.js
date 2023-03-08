7da872684647a7f679a382068415e538dd7fd93f
thomasHeitFux <thomas41392@gmail.com>
2023-03-08 20:33:06 -0300

    config firebase update

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -5,39 +5,22 @@ import "firebase/compat/storage";
 import "firebase/compat/firestore";
 import { getDatabase } from "firebase/database";
 
-// export const app = firebase.initializeApp({
-//   projectId: "zapas-56035",
-//   appId: "1:305266544328:web:a09d4d75e1bf19919c9ffd",
-//   storageBucket: "zapas-56035.appspot.com",
-//   locationId: "southamerica-east1",
-//   apiKey: "AIzaSyBhNLEEhfneLhbVZojVZnPXdSwzBMS0CBc",
-//   authDomain: "zapas-56035.firebaseapp.com",
-//   messagingSenderId: "305266544328",
-//   databaseURL: "https://DATABASE_NAME.firebaseio.com",
-// });
-//-------------zapas
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBhNLEEhfneLhbVZojVZnPXdSwzBMS0CBc",
-  authDomain: "zapas-56035.firebaseapp.com",
-  projectId: "zapas-56035",
-  storageBucket: "zapas-56035.appspot.com",
-  messagingSenderId: "305266544328",
-  appId: "1:305266544328:web:a09d4d75e1bf19919c9ffd"
+  apiKey: "AIzaSyD55AcPqfX6UuQUGakP8yidlVbG4LGi7tU",
+  authDomain: "shoes-c17f7.firebaseapp.com",
+  projectId: "shoes-c17f7",
+  storageBucket: "shoes-c17f7.appspot.com",
+  messagingSenderId: "879707590917",
+  appId: "1:879707590917:web:51c587482513f9bc50e76c",
+  measurementId: "G-GHQ9R954MV"
   // appId: "1:305266544328:web:76c9f917d9405b449c9ffd"
 };
-//---------------zapatillas-proyecto
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAauuDZYUBgVdRjJYAQeKo47RzgmMe38M0",
-//   authDomain: "proyecto-zapatillas-6e2ce.firebaseapp.com",
-//   projectId: "proyecto-zapatillas-6e2ce",
-//   storageBucket: "proyecto-zapatillas-6e2ce.appspot.com",
-//   messagingSenderId: "502350204377",
-//   appId: "1:502350204377:web:5a448f49e111cf51da9dc8",
-//   measurementId: "G-62NF4KXSGW",
-// };
+
 
 export const app = initializeApp(firebaseConfig);
 // const database = getDatabase(app);
 export const auth = getAuth(app);
+
 
 
